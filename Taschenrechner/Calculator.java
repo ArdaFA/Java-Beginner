974bdd7895e925ffcbfb806766502f6feef7976a
ArdaFA <125291182+ArdaFA@users.noreply.github.com>
2023-02-14 13:18:23 +0100

    Update Calculator.java

--- Taschenrechner/Calculator.java
+++ Taschenrechner/Calculator.java
@@ -1,1 +1,35 @@
-
+import java.util.Scanner;
+public class Calculator {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Willkommen zum Java-Taschenrechner");
+		System.out.println("1. Addition (+)");
+		System.out.println("2. Substraktion (-)");
+		
+		System.out.print("Bitte geben Sie die Nummer der Rechneroperationen: ");
+		int c;
+		c = scanner.nextInt();
+		
+		System.out.print("Bitte geben Sie die erste Zahl: ");
+		double a;
+		a = scanner.nextDouble();
+		
+		System.out.print("Bitte geben Sie die zweite Zahl");
+		double b;
+		b = scanner.nextDouble();
+		
+		if ( c == 1 ) {
+			System.out.println("Java Calculator " + (a+b));
+		}
+		
+		else if ( c == 2 ) {
+			System.out.println("Java Calculator " + (a-b));
+		}
+		
+		else {
+			System.out.println("falsche Rechneroperationen oder falsche Zahl");
+		}
+	}
+}
